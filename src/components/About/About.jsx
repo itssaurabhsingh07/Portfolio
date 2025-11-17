435f5b35c601bef81c3f191c4de8911e76748d45
Saurabh Singh <amreshsingh902681@gmail.com>
2025-11-17 14:02:05 +0530

    COmpleted navbar With Button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,13 @@ import './About.css'
 function About() {
   return (
     <div id='about'>
-        
+      <h1>About Us</h1>
+      <p>
+        Welcome to our application! We are dedicated to providing the best service possible. Our team works tirelessly to ensure that our users have a seamless experience.
+      </p>
+      <p>
+        Our mission is to innovate and lead in our industry, constantly improving and adapting to meet the needs of our community. Thank you for being a part of our journey!
+      </p>  
     </div>
   )
 }
